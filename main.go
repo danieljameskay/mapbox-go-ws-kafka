c2cfcad64419f0de6ac98747d52a5b45008fad5e
Danny Kay <danieljameskay@gmail.com>
2018-06-29 19:23:57 +0100

    Changed port settings

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gorilla/websocket"
+	"os"
 )
 
 var upgrader = websocket.Upgrader{
@@ -18,7 +19,7 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	port := 1337
+	port := os.Getenv("port")
 	http.HandleFunc("/feed", liveFeed)
 	log.Printf("Server starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
